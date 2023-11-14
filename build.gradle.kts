e270a5e461e0192e9b4c967f993ec96076ea30dc
qualterz <38355785+qualterz@users.noreply.github.com>
2023-11-14 20:50:17 +0200

    Save slots sorted (#14)

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "io.github.qualterz"
-version = "0.1.2"
+version = "0.1.3"
 
 publishing {
     repositories {
--- src/main/kotlin/io/github/qualterz/cmarkup/data/ContainerMarkup.kt
+++ src/main/kotlin/io/github/qualterz/cmarkup/data/ContainerMarkup.kt
@@ -3,11 +3,14 @@ package io.github.qualterz.cmarkup.data
 import kotlinx.serialization.Serializable
 import org.bukkit.event.inventory.InventoryType
 
+typealias Slot = Int
+typealias SlotKey = String
+
 @Serializable
 data class ContainerMarkup(
     var title: String?,
     var titleColor: String?,
     val size: Int?,
     val type: InventoryType?,
-    val slots: MutableMap<String, MutableSet<Int>>
+    val slots: MutableMap<SlotKey, MutableSet<Slot>>
 )
